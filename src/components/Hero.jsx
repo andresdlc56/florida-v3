fb7ccdb0a8654a3b6422eafb454bddee09eb76c4
30287561jona <davilajesus970@gmail.com>
2023-06-19 16:03:36 -0400

    Se realizaron cambios en About.jsx, Whyus.jsx Y Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ export const Hero = () => {
                 <div className="row justify-content-between gy-5">
                     <div className="col-lg-5 order-2 order-lg-1 d-flex flex-column justify-content-center align-items-center align-items-lg-start text-center text-lg-start">
                     <h2 data-aos="fade-up">Enjoy Your Healthy<br />Delicious Food</h2>
-                    <p data-aos="fade-up" data-aos-delay="100">Sed autem laudantium dolores. Voluptatem itaque ea consequatur eveniet. Eum quas beatae cumque eum quaerat.</p>
+                    <p data-aos="fade-up" data-aos-delay="100">Licorería La Florida, empresa dedicada a la elaboración, envejecimiento y envasado de bebidas alcohólicas.</p>
                     <div className="d-flex" data-aos="fade-up" data-aos-delay="200">
                         <a href="#book-a-table" className="btn-book-a-table">Book a Table</a>
                         <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" className="glightbox btn-watch-video d-flex align-items-center"><i className="bi bi-play-circle"></i><span>Watch Video</span></a>
