be870145ff4f516cbe92df5417d95d17fed2176e
Andres de la Cruz <andresdlc56@gmail.com>
2023-06-21 09:12:51 -0400

    cambiando img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,16 @@
 
 export const Hero = () => {
     return (
-        <section id="hero" className="hero d-flex align-items-center section-bg">
+        <section id="hero" className="hero d-flex align-items-center section-bg-img">
                 <div className="container">
                 <div className="row justify-content-between gy-5">
                     <div className="col-lg-5 order-2 order-lg-1 d-flex flex-column justify-content-center align-items-center align-items-lg-start text-center text-lg-start">
-                    <h2 data-aos="fade-up">Disfruta de un<br />Producto de Calidad</h2>
-                    <p data-aos="fade-up" data-aos-delay="100">Sed autem laudantium dolores. Voluptatem itaque ea consequatur eveniet. Eum quas beatae cumque eum quaerat.</p>
-                    <div className="d-flex" data-aos="fade-up" data-aos-delay="200">
-                        <a href="#book-a-table" className="btn-book-a-table">Book a Table</a>
-                        <a href="https://www.youtube.com/watch?v=LXb3EKWsInQ" className="glightbox btn-watch-video d-flex align-items-center"><i className="bi bi-play-circle"></i><span>Watch Video</span></a>
-                    </div>
+                        <h2 data-aos="fade-up">Disfruta de un<br />Producto de Calidad</h2>
+                        <p data-aos="fade-up" data-aos-delay="100" style={{ color: "white" }}>Sed autem laudantium dolores. Voluptatem itaque ea consequatur eveniet. Eum quas beatae cumque eum quaerat.</p>
+                        
                     </div>
                     <div className="col-lg-5 order-1 order-lg-2 text-center text-lg-start">
-                    <img src="./src/assets/img/hero-img.png" className="img-fluid" alt="" data-aos="zoom-out" data-aos-delay="300" />
+                    
                     </div>
                 </div>
                 </div>
