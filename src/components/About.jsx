f330123642000d32dbf93ca60315111c311d5092
30287561jona <davilajesus970@gmail.com>
2023-07-03 16:03:46 -0400

    Cambios En El index.html Y main.css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,24 +10,28 @@ export const About = () => {
                         </div>
 
                         <div className="row gy-4">
-                            <div className="col-lg-7 position-relative about-img" style={{ backgroundImage: "url(/img/Captura5.PNG)" }} data-aos="fade-up" wi data-aos-delay="150">
+                            <div className="col-lg-7 position-relative about-img" style={{ backgroundImage: "url(src/assets/img/dioriente.PNG)" }} data-aos="fade-up" wi data-aos-delay="150">
                                
                             </div>
                             <div className="col-lg-5 d-flex align-items-start" data-aos="fade-up" data-aos-delay="300">
                                 <div className="content ps-0 ps-lg-5">
                                     <p className="fst-italic">
                                     Licorería La Florida, empresa dedicada a la elaboración, envejecimiento y envasado de bebidas alcohólicas, fue fundada en 1948 por 
-                                    Don Rafael en la ciudad de Cumaná, primera ciudad erigida por los españoles en tierras americanas.
+                                    Don Rafael en la ciudad de Cumaná.
                                     </p>
                                     <ul>
                                         <li><i className="bi bi-check2-all"></i> La empresa es hoy por hoy una de las más representativas del estado Sucre, proyectando su imagen dentro y fuera de nuestras fronteras.</li>
                                         <li><i className="bi bi-check2-all"></i> La excelente calidad de sus productos la ha hecho merecedora de numerosos reconocimientos nacionales e internacionales.</li>
-                                        <li><i className="bi bi-check2-all"></i> Licorería La Florida une hoy los más avanzados recursos tecnológicos, los cuales, junto a la mística de las personas que intervienen en el proceso, permiten obtener 
-                                        productos de alta calidad e insuperable sabor y aroma.</li>
+                                        <li><i className="bi bi-check2-all"></i> Licorería La Florida une hoy los más avanzados recursos tecnológicos, los cuales, permiten obtener productos de alta calidad e insuperable sabor y aroma.</li>
+                                        
+                                        <ul className="text-center">
+                                            <img src="src/assets/img/Medallas/Medalla3.png" style={{ height: "135px" }} alt="" />
+                                            <img src="src/assets/img/Medallas/medalla2.jpg" style={{ height: "135px" }} alt="" />
+                                            <img src="src/assets/img/Medallas/medalla2.jpg" style={{ height: "135px" }} alt="" /> 
+                                        </ul>
+                                        
                                     </ul>
-
-                                    <img src="/img/medallas/medalla-1.png" style={{ height: "170px" }} alt="" />
-                                    <img src="/img/medallas/medalla-1.png" style={{ height: "170px" }} alt="" />
+                                
                                 </div>
                             </div>
                         </div>
